aa869f89aa0668991aaf5d8d3a07473026cf02c3
reksarka <reksarka@gmail.com>
2021-06-13 13:20:47 +0300

    VS project builder uses abs paths now.

--- win/win32/vs/build-project.bat
+++ win/win32/vs/build-project.bat
@@ -1,14 +1,22 @@
 @echo off
 
-rem Rough cleaning of build results instead of `msbuild /t:Clean` to get rid of 
+rem The folder of the current batch file.
+set vs_path=%~d0%~p0
+
+rem ---------------------------------------------------------------------------
+
+rem Remove generated dirs roughly, instead of `msbuild /t:Clean` to get rid of 
 rem additional dependencies.
-set root=..\..\..
-set build_dirs=%root%\bin %root%\tools obj symbols
+set root_path=%vs_path%..\..\..
+set build_dirs=%root_path%\bin %root_path%\tools %vs_path%obj %vs_path%symbols
+
 for %%i in (%build_dirs%) do (
   rmdir /s /q %%i 2>NUL
 )
 
-call "vscmd\init.bat" -x 64 -toolset || exit
+rem ---------------------------------------------------------------------------
+
+call "%vs_path%vscmd\init.bat" -x 64 -toolset || goto :EOF
 
 rem `Platform` should be set by `vcvars`.
 if "%Platform%" == "x86" (
@@ -19,6 +27,6 @@ if "%Platform%" == "x86" (
 
 set Project=NetHack
 
-msbuild NetHack.sln ^
+msbuild %vs_path%NetHack.sln ^
   /t:%Project%:rebuild ^
   /p:Configuration=Release;Platform=%obsolete_platform_name%
