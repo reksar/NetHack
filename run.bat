aa01a08991a1caa39bc5c0cc6f001a6c8803db6f
reksarka <reksarka@gmail.com>
2021-06-18 20:57:42 +0300

    Deletes saved progress before run.

--- run.bat
+++ run.bat
@@ -3,10 +3,30 @@
 set NetHackRoot=%~d0%~p0
 set NetHack=%NetHackRoot%bin\Release\x64\NetHack.exe
 set build=%NetHackRoot%win\win32\vs\build-project.bat
+set uname=you
+
+rem ---------------------------------------------------------------------------
+rem Build the NetHack if it does not exist.
 
 if not exist "%NetHack%" (
   echo NetHack does not exist, trying to build ...
   %build%
 )
 
-%NetHack% -u you -@
+rem ---------------------------------------------------------------------------
+rem Delete saved progress to fresh start.
+
+for /f "tokens=2 delims==" %%i in ('
+  %NetHack% --showpaths ^| findstr savedir
+') do (
+  set savedir=%%~i
+)
+
+set SavedProgress=%savedir%%uname%.*
+
+if exist "%SavedProgress%" (
+  del "%SavedProgress%"
+)
+
+rem ---------------------------------------------------------------------------
+%NetHack% -u %uname% -@
